0a560a88cff4ce93ef382044f6bb3c662cdc8d3b
Brenton Alker <brenton@tekerson.com>
2015-07-08 23:13:51 +1000

    Remove most of the scaffolding

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,28 +1,15 @@
-/* global malarkey:false, toastr:false, moment:false */
+/* global moment:false */
 import config from './index.config';
 
 import routerConfig from './index.route';
-
-import runBlock from './index.run';
-import MainController from './main/main.controller';
-import GithubContributorService from './components/githubContributor/githubContributor.service';
-import WebDevTecService from './components/webDevTec/webDevTec.service';
-import MalarkeyDirective from './components/malarkey/malarkey.directive';
 
 import './templates.module';
 import './bookmarks/bookmarks.module';
 import './layout/layout.module';
 
 angular.module('ngBookmarks', ['templates', 'app.layout', 'ngRoute', 'ui.bootstrap', 'bookmarks'])
-  .constant('malarkey', malarkey)
-  .constant('toastr', toastr)
   .constant('moment', moment)
   .config(config)
 
   .config(routerConfig)
-
-  .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
-  .controller('MainController', MainController)
-  .directive('acmeMalarkey', () => new MalarkeyDirective(malarkey));
+;
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -2,11 +2,6 @@ function routerConfig ($routeProvider) {
   'ngInject';
   $routeProvider
     .when('/', {
-      templateUrl: 'app/main/main.html',
-      controller: 'MainController',
-      controllerAs: 'main'
-    })
-    .when('/bookmarks', {
       templateUrl: 'app/bookmarks/bookmarks.html',
       controller: 'BookmarksController',
       controllerAs: 'vm'
