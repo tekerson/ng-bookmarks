4ed2d386199f764da84211bb09b672d60c4b6a2a
Brenton Alker <brenton@tekerson.com>
2015-07-26 22:41:31 +1000

    Move validation checks into the constructor of Bookmark Entity

--- src/app/bookmarks/bookmark/bookmark.entity.js
+++ src/app/bookmarks/bookmark/bookmark.entity.js
@@ -3,31 +3,29 @@ import * as Fields from './fields';
 
 class Bookmark {
   constructor(id, fields) {
+    if (!Id.assertType(id)) {
+      return new TypeError('Expected:Id');
+    }
+
+    if (!Fields.assertType(fields)) {
+      return new TypeError('Expected:Fields');
+    }
+
     this.id = id;
     this.fields = fields;
     return Object.freeze(this);
   }
 }
 
-function mkBookmark(id, fields) {
-  if (!Id.assertType(id)) {
-    return new TypeError('Expected:Id');
-  }
-
-  if (!Fields.assertType(fields)) {
-    return new TypeError('Expected:Fields');
-  }
-
-  return new Bookmark(id, fields);
-}
-
-export var fromJSON = obj =>
-  mkBookmark(
+export function fromJSON(obj) {
+  return new Bookmark(
     Id.fromString(obj.id),
     Fields.fromJSON(obj));
+}
 
-export var fromObject = obj =>
-  mkBookmark(obj.id, obj.fields);
+export function fromObject(obj) {
+  return new Bookmark(obj.id, obj.fields);
+}
 
 export function assertType(obj) {
   return obj instanceof Bookmark;
